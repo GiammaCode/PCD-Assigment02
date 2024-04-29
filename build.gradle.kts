51c6fbc49abb9d4aaad339c181725cea3f8ad6be
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-04-29 13:31:33 +0200

    add vertex dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //add vert.x module
+    implementation("io.vertx:vertx-core:4.5.7")
+    implementation("io.vertx:vertx-web:4.5.7")
+    implementation("io.vertx:vertx-web-client:4.5.7")
 }
 
 tasks.test {
